2a4bbd52c949d7e5398c6758c1bd34d6232f33b2
Pilleow <pilleowo@gmail.com>
2025-02-28 22:55:12 +0100

    updated player to smooth camera and turning

--- gjgh2025_dzinsy_jakigorsewa/assets/scenes/prefabs/player.gd
+++ gjgh2025_dzinsy_jakigorsewa/assets/scenes/prefabs/player.gd
@@ -15,11 +15,12 @@ var wheel_angle_limit = [-90.0, 90.0]
 
 @onready var carCollider: CollisionShape2D = $CarCollision
 @onready var carSprite: Sprite2D = $CarSprite
+@onready var camera: Camera2D = $Camera2D
 
 func _steer_set(sang: float):
 	if sang < steering_angle_limit[0] or sang > steering_angle_limit[1]:
 		return false
-	steering_angle = sang / 20
+	steering_angle = move_toward(steering_angle, sang / 20, 0.005)
 	return true
 
 func _accel_set(acc: float):
@@ -35,7 +36,7 @@ func _move(timedelta: float):
 	if abs(car_speed) > car_max_speed:
 		car_speed = car_max_speed * sign(car_speed)
 	car_speed *= 1 - car_ground_friction
-	car_angle += steering_angle
+	car_angle += steering_angle * car_speed / car_max_speed
 	velocity = Vector2(car_speed * sin(car_angle), car_speed * cos(car_angle))
 	
 	var lslidecol = get_last_slide_collision()
@@ -50,10 +51,15 @@ func _take_input():
 	var acceleration = Input.get_axis("up", "down") * car_max_accel
 	_accel_set(acceleration)
 	var rotation = Input.get_axis("left", "right")
-	_steer_set(rotation * -1)
+	_steer_set(rotation)
+
+func _update_camera_ahead_of_car():
+	var mod = -velocity * abs(car_speed) / car_max_speed * 0.7
+	camera.global_position = global_position - Vector2(478.8, 0) - mod
 
 func _physics_process(delta):
 	carCollider.rotation = -car_angle
 	carSprite.rotation = -car_angle
 	_take_input()
 	_move(delta)
+	_update_camera_ahead_of_car()
