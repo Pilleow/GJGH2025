bc3b266df0dcf0a487b629b5a4f3a8525f5505a6
Pilleow <pilleowo@gmail.com>
2025-03-02 16:11:59 +0100

    added both cutscenes

--- gjgh2025_dzinsy_jakigorsewa/assets/scenes/levels/cutscenka_end.gd
+++ gjgh2025_dzinsy_jakigorsewa/assets/scenes/levels/cutscenka_end.gd
@@ -2,7 +2,10 @@ extends Node2D
 
 @onready var animSprite = $AnimatedSprite2D
 
+func _ready():
+	animSprite.play("default")
+
 func _process(delta):
 	if not animSprite.is_playing():
 		GlobalData.prev_scene = get_tree().current_scene.scene_file_path
-		get_tree().change_scene_to_file("res://assets/scenes/levels/level_1.tscn")
+		get_tree().change_scene_to_file("res://assets/scenes/menu/menu.tscn")
--- gjgh2025_dzinsy_jakigorsewa/assets/scenes/levels/cutscenka_start.gd
+++ gjgh2025_dzinsy_jakigorsewa/assets/scenes/levels/cutscenka_start.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 @onready var animSprite = $AnimatedSprite2D
 
+func _ready():
+	animSprite.play("default")
+
 func _process(delta):
 	if not animSprite.is_playing():
 		GlobalData.prev_scene = get_tree().current_scene.scene_file_path
